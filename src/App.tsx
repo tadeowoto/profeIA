b50f30301d467d3b4b78fcbac2fbad84f7744505
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-06-12 13:54:22 -0300

    feat: add useState for messages, loading and asignature, types too

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import { useState } from "react";
+
+type Message = {
+  id: string;
+  content: string;
+  role: "user" | "assistant";
+};
+type Asignature = {
+  id: string;
+  name:
+    | "Matemáticas"
+    | "Ciencias"
+    | "Lengua"
+    | "Historia"
+    | "Geografía"
+    | "Ciencias Sociales"
+    | "Computación"
+    | "Astronomía";
+};
+
 function App() {
-  return <h1>ProfeIA</h1>;
+  const [message, setMessage] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [selectedAsignature, setSelectedAsignature] =
+    useState<Asignature | null>(null);
+
+  return (
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-full">
+        <h1>ProfeIA</h1>
+        <form action="">
+          <select name="asignature" id="asignature">
+            <option value="matemáticas">Matemáticas</option>
+            <option value="ciencias">Ciencias</option>
+            <option value="lengua">Lengua</option>
+            <option value="historia">Historia</option>
+            <option value="geografía">Geografía</option>
+            <option value="ciencias-sociales">Ciencias Sociales</option>
+            <option value="computación">Computación</option>
+            <option value="astronomía">Astronomía</option>
+          </select>
+          <textarea name="question" id="question"></textarea>
+          <button>Enviar</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default App;
